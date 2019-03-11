f74890076773ec01d34232fc8d29eb97f00c4eb6
Alli Wong <wonga10@gmail.com>
2019-03-10 19:23:37 -0700

    add plot, mean, std

--- src/agent.py
+++ src/agent.py
@@ -1,5 +1,6 @@
 import numpy as np
 import src.environment as env
+import matplotlib.pyplot as plt
 from src.grid import Grid
 from src.policy import Policy
 
@@ -11,41 +12,45 @@ class Problem:
         self.policy = Policy(env.ACTIONS, 0.2, 0.9, 0.1)
         self.robby = Agent(self.grid, np.random.randint(0, row_sz), np.random.randint(0, col_sz), self.policy)
         self.epoch = 1
+        self.rewards_per_episode = []
 
     def train(self,steps):
         for i in range(steps):
-            #print(i)
             self.robby.take_action()
         return self.robby.total_reward
 
     def run(self, n, m):
-        print("Training--------------\n")
-        print("Training--------------\n")
-        print("Training--------------\n")
+        print("Beginning Training...\n")
         for i in range(n): #episodes
             reward_accumulated = self.train(m)
             self.policy.update_e(self.epoch)
             self.epoch += 1
             self.grid = Grid(10,10)
             self.robby = Agent(self.grid, np.random.randint(0, 10), np.random.randint(0, 10), self.policy)
+            if self.epoch % 100 == 0:
+                self.rewards_per_episode.append(reward_accumulated)
             print(i, self.policy._e, reward_accumulated)
-        print("Test---------------\n")
-        print("Test---------------\n")
-        print("Test---------------\n")
+        self._create_training_plot()
+        print("Beginning Test...\n")
         self.policy.reset_e()
+        self.rewards_per_episode = []
         for i in range(n):
             self.grid = Grid(10,10)
             self.robby = Agent(self.grid, np.random.randint(0, 10), np.random.randint(0, 10), self.policy)
             reward_accumulated = self.train(m)
+            self.rewards_per_episode.append(reward_accumulated)
             print(i, self.policy._e, reward_accumulated)
-            if reward_accumulated > 350:
-                print(self.grid.grid)
-        self.grid = Grid(10,10)
-        print(self.grid.grid)
-        self.robby = Agent(self.grid, np.random.randint(0, 10), np.random.randint(0, 10), self.policy)
-        reward_accumulated = self.train(m)
-        print(self.policy._e, reward_accumulated)
-        print(self.grid.grid)
+        return self._calculate_test_mean_std()
+
+    def _create_training_plot(self):
+        episodes = [x*100 for x in range(1,51)]
+        plt.plot(episodes, self.rewards_per_episode, 'ro')
+        plt.savefig('training_plot.png', bbox_inches='tight')
+
+    def _calculate_test_mean_std(self):
+        mean = np.mean(self.rewards_per_episode)
+        std = np.std(self.rewards_per_episode)
+        return mean, std
 
 
 class Agent:
@@ -67,5 +72,5 @@ class Agent:
 
 p = Problem(10,10)
 #p.train(200)
-p.run(5000,200)
+print(p.run(5000,200))
 
